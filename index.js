d5c24e5c559f6fae35e3ef0445a4879ff52eb051
fladeia <252842.5s@gmail.com>
2021-08-27 11:58:34 -0300

    todo illustration, arrow toggle

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const arrowToggles = document.querySelectorAll(".arrow-toggle");
-const answers = document.querySelectorAll(".answer")
 
 arrowToggles.forEach((arrowToggle) => {
-  arrowToggle.addEventListener("click", (e) => {
-    console.log(arrowToggle.parentNode.children[1])
-    arrowToggle.parentNode.children[1].classList.toggle("answer-toggle")
-  });
+  arrowToggle.addEventListener("click", () => {
+    arrowToggles.forEach((arrowToggle) => {
+      arrowToggle.parentNode.children[1].classList.add("answer-toggle")
+    })
+      arrowToggle.parentNode.children[1].classList.toggle("answer-toggle")
+    })
 });
-
-// answers.forEach((answer) => {
-//   answer.addEventListener("click", () => {
-//     answer.classList.toggle("answer-toggle")
-//   })
-// })
