1b80d27c7df07a8ad5dc45651844470bcaed1811
fladeia <252842.5s@gmail.com>
2021-08-28 10:06:07 -0300

    toggle changed js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const arrowToggles = document.querySelectorAll(".arrow-toggle");
+const questions = document.querySelectorAll(".questions");
 
-arrowToggles.forEach((arrowToggle) => {
-  arrowToggle.addEventListener("click", () => {
-    arrowToggles.forEach((arrowToggle) => {
-      arrowToggle.parentNode.children[1].classList.add("answer-toggle")
-    })
-      arrowToggle.parentNode.children[1].classList.toggle("answer-toggle")
-    })
+// function resetArrowToggle() {
+//   questions.forEach((arrowToggle) => {
+//     arrowToggle.parentNode.children[1].classList.add("answer-toggle");
+//     arrowToggle.style.transform = "scaleY(1)";
+//   });
+// }
+
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    // resetArrowToggle();
+    question.lastElementChild.lastElementChild.classList.toggle(
+      "answer-toggle"
+    );
+    question.firstElementChild.firstElementChild.classList.toggle(
+      "answer-toggle-font"
+    );
+    question.lastElementChild.children[0].classList.toggle("arrow-toggle-up");
+  });
 });
